73c4dbf8e5dc6061ea0702113ac551d8d6b9336f
AmirDaneshkar <amir.daneshkar@hotmail.com>
2019-09-01 16:39:04 +0430

    making get, post, delete requests to external apis (jsonplaceholder.com , the post and delete request work but they wont submit to server)

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
   import Header from './components/layouts/Header';
   import Todos from './components/Todos';
   import AddTodo from './components/AddTodo';
+  import axios from 'axios';
 
 export default {
   name: 'app',
@@ -21,34 +22,34 @@ export default {
 
   data(){
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: true
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo three",
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
 
   methods: {
     deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res=> this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err));
     },
 
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      }).then(res => this.todos = [...this.todos, res.data])
+      .catch(err => console.log(err));
     }
+  },
+
+  //this will run when the file is loaded
+  created(){
+    //sending request to external Api
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.todos = res.data )
+      .catch(err => console.log(err));
   }
 }
 </script>

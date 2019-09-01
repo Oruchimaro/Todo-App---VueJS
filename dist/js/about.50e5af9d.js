c31d9a4149f50a8bb35262c25e024873f54381e5
AmirDaneshkar <amir.daneshkar@hotmail.com>
2019-09-01 17:13:23 +0430

    build the project (from ui,its easy) so you get the dist folder ,it will be used for publishing

---
+++ dist/js/about.50e5af9d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},n=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("About:")]),s("p",[t._v("This is a todo list app v1.0.0, From Traversy Media crash course on Vue, 2019")])])}],r=s("2877"),o={},u=Object(r["a"])(o,a,n,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=about.50e5af9d.js.map
